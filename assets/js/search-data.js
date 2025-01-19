264a3af803d90418cf8eee5a5d08a7b0e7e0c738
nicop111 <82090042+nicop111@users.noreply.github.com>
2025-01-19 14:30:14 +0000

    Deploying to gh-pages from @ nicop111/nicopedia@da7ef785712d5ebc5de33bbe57a82aa260c93071 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,14 +7,14 @@ ninja.data = [{
     title: "",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/al-folio-test/";
+      window.location.href = "/nicopedia/";
     },
   },{id: "nav-about",
           title: "about",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/al-folio-test/about/";
+            window.location.href = "/nicopedia/about/";
           },
         },{id: "post-dpop",
       
@@ -24,7 +24,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/al-folio-test/2025/DPOP/";
+          window.location.href = "/nicopedia/2025/DPOP/";
         
       },
     },{id: "news-a-simple-inline-announcement",
@@ -34,7 +34,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/al-folio-test/news/announcement_2/";
+              window.location.href = "/nicopedia/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
@@ -42,47 +42,47 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio-test/projects/1_project/";
+              window.location.href = "/nicopedia/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio-test/projects/2_project/";
+              window.location.href = "/nicopedia/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio-test/projects/3_project/";
+              window.location.href = "/nicopedia/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio-test/projects/4_project/";
+              window.location.href = "/nicopedia/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio-test/projects/5_project/";
+              window.location.href = "/nicopedia/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio-test/projects/6_project/";
+              window.location.href = "/nicopedia/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio-test/projects/7_project/";
+              window.location.href = "/nicopedia/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio-test/projects/8_project/";
+              window.location.href = "/nicopedia/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio-test/projects/9_project/";
+              window.location.href = "/nicopedia/projects/9_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
